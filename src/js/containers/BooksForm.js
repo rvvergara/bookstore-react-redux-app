df84962e18b3f9ecb0902961c89f9d6f25884d23
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-24 15:57:55 +0600

    Add style in form
    - Initial style for form input and select

--- src/js/containers/BooksForm.js
+++ src/js/containers/BooksForm.js
@@ -1,66 +1,73 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { addBook, changeFilters } from "../actions";
-import categories from "../data/bookCategories";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addBook, changeFilters } from '../actions';
+import categories from '../data/bookCategories';
 
 class BooksForm extends React.Component {
   state = {
-    title: "",
-    category: "Select Category"
+    title: '',
+    category: 'Select Category',
   };
 
   handleChange = (key, val) =>
     this.setState(() => ({
-      [key]: val
+      [key]: val,
     }));
 
   handleSubmit = () => {
     this.props.addBook({
       title: this.state.title,
-      category: this.state.category
+      category: this.state.category,
     });
-    this.props.changeFilters("All");
+    this.props.changeFilters('All');
     this.setState(() => ({
-      title: "",
-      category: "Select Category"
+      title: '',
+      category: 'Select Category',
     }));
   };
 
   render() {
     return (
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          this.handleSubmit();
-        }}
-      >
-        <input
-          type="text"
-          value={this.state.title}
-          onChange={e => this.handleChange("title", e.target.value)}
-        />
-        <select
-          value={this.state.category}
-          onChange={e => this.handleChange("category", e.target.value)}
+      <div>
+        <h2 className="add-new-title">Add new book</h2>
+        <form
+          className="form"
+          onSubmit={e => {
+            e.preventDefault();
+            this.handleSubmit();
+          }}
         >
-          {["Select Category"].concat(categories).map(category => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-        <button type="submit">Add Book</button>
-      </form>
+          <input
+            className="form-control"
+            type="text"
+            value={this.state.title}
+            onChange={e => this.handleChange('title', e.target.value)}
+          />
+          <select
+            value={this.state.category}
+            onChange={e => this.handleChange('category', e.target.value)}
+          >
+            {['Select Category'].concat(categories).map(category => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          <button className="add-book-btn" type="submit">
+            Add Book
+          </button>
+        </form>
+      </div>
     );
   }
 }
 
 BooksForm.propTypes = {
   addBook: PropTypes.func.isRequired,
-}
+};
 
 export default connect(
   null,
-  { addBook, changeFilters }
+  { addBook, changeFilters },
 )(BooksForm);
