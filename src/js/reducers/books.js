83057b74f4218bfa86b7b93c47257727a23f88d9
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-24 08:51:53 +0600

    Add REMOVE_BOOK case to books reducer

--- src/js/reducers/books.js
+++ src/js/reducers/books.js
@@ -1,9 +1,11 @@
-import books from '../data/dummyBooks';
+import books from "../data/dummyBooks";
 
 export default (state = books, action) => {
-  switch(action.type){
-    case 'ADD_BOOK':
+  switch (action.type) {
+    case "ADD_BOOK":
       return [...state, action.book];
+    case "REMOVE_BOOK":
+      return state.filter(book => book.id !== action.book.id);
     default:
       return state;
   }
