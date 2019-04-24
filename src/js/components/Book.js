018ab480bb4f1027ea24540d7a811794580861b7
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-24 12:02:37 +0600

    Remove td and implement new structure for Book

--- src/js/components/Book.js
+++ src/js/components/Book.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Book = (props) => {
+const Book = props => {
   const { id, title, category } = props.book;
   const { handleRemove } = props;
   return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-      <td>
-        <button type="button" onClick={() => handleRemove(id)}>
-          Remove
-        </button>
-      </td>
-    </tr>
+    <div className="card">
+      <div className="card-body">
+        <div className="columns">
+          <div className="col">
+            <h4 className="genre">{category}</h4>
+            <h3 className="title">{title}</h3>
+            <div className="action">
+              <button type="button" onClick={() => handleRemove(id)}>
+                Remove
+              </button>
+            </div>
+          </div>
+          <div className="col" />
+        </div>
+      </div>
+    </div>
   );
 };
 
 Book.propTypes = {
-  handleRemove: PropTypes.func.isRequired,
+  handleRemove: PropTypes.func.isRequired
 };
 
 export default Book;
