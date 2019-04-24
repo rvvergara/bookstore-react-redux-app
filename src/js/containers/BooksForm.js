fb64feb550465ec50fbbb94672409c0ce7c71912
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-24 11:24:15 +0800

    Handle book adding by connecting form to redux store

--- src/js/containers/BooksForm.js
+++ src/js/containers/BooksForm.js
@@ -1,31 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { addBook } from '../actions'
 import categories from '../data/bookCategories';
 
-const BooksForm = () => {
-  return (
-    <form>
-      <input type="text"/>
-      <select>
-        {
-          categories.map(category => (
-            <option
-              key={category}
-              value={category}
-            >
-              { category }
-            </option>
-          ))
-        }
-      </select>
-      <button 
-        type="submit"      
-      >
-        Add Book
-      </button>
-    </form>
-  )
-};
+class BooksForm extends React.Component {
+  state = {
+    title:'',
+    category: 'Action',
+  }
+  
+  handleChange = (key, val) => this.setState(() => ({
+    [key]: val,
+  }));
 
-export default BooksForm;
+  handleSubmit = () => {
+    this.props.addBook({title: this.state.title, category: this.state.category});
+    this.setState(() => ({
+      title: '',
+      category: 'Action',
+    }));
+  }
+
+  render(){
+    return (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          this.handleSubmit()
+        }}
+      >
+        <input 
+          type="text"
+          value={this.state.title}
+          onChange={e => this.handleChange('title', e.target.value)}
+          />
+        <select
+          value={this.state.category}
+          onChange={e => this.handleChange('category', e.target.value)}
+        >
+          {
+            categories.map(category => (
+              <option
+                key={category}
+                value={category}
+              >
+                { category }
+              </option>
+            ))
+          }
+        </select>
+        <button 
+          type="submit"      
+        >
+          Add Book
+        </button>
+      </form>
+    );
+  }
+}
+
+export default connect(null, { addBook })(BooksForm);
