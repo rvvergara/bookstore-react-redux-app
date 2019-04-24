30e7e041b64fd47d09fe42115ff892a054f1003d
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-24 11:52:55 +0600

    Bookstore app

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Header = () => (
-  <header>
-    <h1>Bookstore App</h1>
+  <header className="panel-bg">
+    <h1 className="Bookstore-CMS">Bookstore App</h1>
   </header>
 );
 
