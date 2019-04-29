a83ea6bb99728f535d61ab7e86d198f8139bbb46
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-29 10:28:58 +0800

    Write tests for bookReducer

---
+++ src/js/test/reducers/books.test.js
@@ -0,0 +1,32 @@
+import booksReducer from '../../reducers/books';
+import books from '../fixtures/books';
+
+let bookState;
+
+beforeEach(() => {
+  bookState = books;
+});
+
+test('it should return default state', () => {
+  const action = {
+    type: 'CHANGE_FILTER',
+    filterText: 'Cow',
+  };
+  expect(booksReducer(bookState, action)).toEqual(bookState);
+});
+
+test('it should add new book', () => {
+  const action = {
+    type: 'ADD_BOOK',
+    book: {
+      title: 'In Praise of Folly',
+      category: 'Comedy',
+    },
+  };
+  expect(booksReducer(bookState, action)).toEqual([...bookState, action.book]);
+});
+
+test('it should remove an existing book', () => {
+  const action = { type: 'REMOVE_BOOK', id: 0 };
+  expect(booksReducer(bookState, action)).toEqual(bookState.slice(1));
+});
