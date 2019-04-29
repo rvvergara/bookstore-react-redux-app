7509a16ca92a3ed9dd424297c98ed6c25e744036
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-29 09:59:25 +0800

    Install and configure enzyme
    1. Install in devDependencies enzyme, enzyme-adapter-react-16 and
    enzyme-to-json
    2. Add the ff line to package.json:
    "jest": {
      "snapshotSerializers": ["enzyme-to-json/serializer"]
    }
    3. Create a src/setupTests.js file containing code to use enzyme adapter
    4. Create test for Book.js to confirm success of setup

---
+++ src/js/test/components/Book.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Book from '../../components/Book';
+
+describe('Book component', () => {
+  let wrapper;
+  let book;
+  let handleRemove;
+  beforeEach(() => {
+    book = {
+      id: 1,
+      title: 'Antiquities of the Jews',
+      category: 'History',
+    };
+    handleRemove = jest.fn();
+    wrapper = shallow(<Book book={book} handleRemove={handleRemove} />);
+  });
+
+  test('it should render the Book component correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
