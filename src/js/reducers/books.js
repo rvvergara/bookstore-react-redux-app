6d31847071f8738bd163fd0e07032e89c3bdcbb1
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-05-27 12:33:52 +0800

    Fix reducer files for proper directory structure

--- src/js/reducers/books.js
+++ src/js/reducers/books.js
@@ -1,10 +1,11 @@
+import { ADD_BOOK, REMOVE_BOOK } from '../actions/actionTypes';
 import books from '../data/dummyBooks';
 
 export default (state = books, action) => {
   switch (action.type) {
-    case 'ADD_BOOK':
+    case ADD_BOOK:
       return [...state, action.book];
-    case 'REMOVE_BOOK':
+    case REMOVE_BOOK:
       return state.filter(book => book.id !== action.id);
     default:
       return state;
