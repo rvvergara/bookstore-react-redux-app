a769677096501d0020bd6e0692d1a91d0961ee05
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-05-31 14:48:40 +0800

    Write test for ProgressUpdateBody

---
+++ src/js/test/components/ProgressUpdateBody.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ProgressUpdateBody from '../../components/ProgressUpdateBody';
+
+describe('ProgressUpdateBody', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const chaptersArray = [1, 2, 3, 4, 5];
+    wrapper = shallow(
+      <ProgressUpdateBody
+        chaptersArray={chaptersArray}
+        id="someId"
+        title="Some Title"
+      />,
+    );
+  });
+
+  test('should render correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should handle change in chapter', () => {
+    wrapper.find('select').prop('onChange')({
+      target: {
+        name: 'current',
+        value: '3',
+      },
+    });
+
+    expect(wrapper.state('current')).toBe('3');
+  });
+});
