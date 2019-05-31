34e7f44ee17142cce7e4df706bc4e3d81f565673
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-05-31 15:22:53 +0800

    Add updating book chapter to books reducer

--- src/js/reducers/books.js
+++ src/js/reducers/books.js
@@ -1,4 +1,4 @@
-import { ADD_BOOK, REMOVE_BOOK } from '../actions/actionTypes';
+import { ADD_BOOK, REMOVE_BOOK, UPDATE_CHAPTER } from '../actions/actionTypes';
 import books from '../data/dummyBooks';
 
 export default (state = books, action) => {
@@ -7,6 +7,14 @@ export default (state = books, action) => {
       return [...state, action.book];
     case REMOVE_BOOK:
       return state.filter(book => book.id !== action.id);
+    case UPDATE_CHAPTER:
+    {
+      const { id, newChapter } = action;
+      const bookIndex = state.findIndex(book => book.id === id);
+      const newState = [...state];
+      newState[bookIndex] = { ...newState[bookIndex], currentChapter: newChapter };
+      return newState;
+    }
     default:
       return state;
   }
--- src/js/test/reducers/books.test.js
+++ src/js/test/reducers/books.test.js
@@ -1,5 +1,7 @@
+import { testNameToKey } from 'jest-snapshot/build/utils';
 import booksReducer from '../../reducers/books';
 import books from '../fixtures/books';
+import { UPDATE_CHAPTER } from '../../actions/actionTypes';
 
 let bookState;
 
@@ -29,4 +31,13 @@ test('it should add new book', () => {
 test('it should remove an existing book', () => {
   const action = { type: 'REMOVE_BOOK', id: '0' };
   expect(booksReducer(bookState, action)).toEqual(bookState.slice(1));
+});
+
+test('it should update current chapter of selected book', () => {
+  const action = {
+    type: UPDATE_CHAPTER,
+    id: bookState[1].id,
+    newChapter: '15',
+  };
+  expect(booksReducer(bookState, action)[1].currentChapter).toBe('15');
 });
