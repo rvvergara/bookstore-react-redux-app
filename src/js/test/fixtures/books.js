c63c1f434770e5138109dde75d50ad90d83b82ba
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-05-31 15:00:00 +0800

    Write tests for ProgressUpdateModal

--- src/js/test/fixtures/books.js
+++ src/js/test/fixtures/books.js
@@ -2,16 +2,25 @@ export default [
   {
     id: 0,
     title: 'Lord of the Rings',
+    author: 'Tolkien',
+    chapters: '35',
     category: 'Action',
+    currentChapter: '',
   },
   {
     id: 1,
     title: 'Guide to Investing',
+    author: 'Kiyosaki',
+    chapters: '24',
+    currentChapter: '',
     category: 'Learning',
   },
   {
     id: 2,
     title: 'Star Trek',
+    author: 'Roddenberry',
+    chapters: '30',
+    currentChapter: '',
     category: 'Sci-Fi',
   },
 ];
