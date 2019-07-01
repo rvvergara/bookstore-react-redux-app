8a920e6c91d6ea034da067dd170cb6d77e9708b4
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-01 18:44:28 +0800

    Write test for setCurrentUser action

---
+++ src/js/test/actions/currentUser.test.js
@@ -0,0 +1,13 @@
+import { SET_CURRENT_USER } from '../../actions/actionTypes';
+import { setCurrentUser } from '../../actions/currentUser';
+import users from '../fixtures/users';
+
+describe('setCurrentUser', () => {
+  test('it returns the right action', () => {
+    const ryto = users[0];
+    expect(setCurrentUser({ authenticated: true, data: ryto })).toEqual({
+      type: SET_CURRENT_USER,
+      currentUser: { authenticated: true, data: ryto },
+    });
+  });
+});
---
+++ src/js/test/fixtures/users.js
@@ -0,0 +1,12 @@
+import uuid from 'uuid';
+
+export default [
+  {
+    id: uuid(),
+    username: 'ryto',
+    email: 'ryto@gmail.com',
+    first_name: 'Ryto',
+    last_name: 'Verkar',
+    token: 'someToken123',
+  },
+];
