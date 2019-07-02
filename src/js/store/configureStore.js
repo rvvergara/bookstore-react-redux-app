766840cef537e8f51da013a3f7191a0d7e5a65fe
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-02 17:21:57 +0800

    Fix middleware issue that happens in App.test.js

--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -2,10 +2,14 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const middleWares = process.env.NODE_ENV === 'development' ? [applyMiddleware(thunk), devTools] : [applyMiddleware(thunk)];
+
 export default () => {
   const store = createStore(
     rootReducer,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
+    compose(...middleWares),
   );
   return store;
 };
