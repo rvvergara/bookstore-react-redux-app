9e34b7277404125f01b6bf161824190caa601dca
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-02 15:41:40 +0800

    Set store in App.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './js/components/App';
-import configureStore from './js/store/configureStore';
 import * as serviceWorker from './serviceWorker';
 
-const store = configureStore();
-
-const connectedApp = (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
-ReactDOM.render(connectedApp, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
+import decode from 'jwt-decode';
 import BooksDashboard from './BooksDashboard';
 import '../../scss/main.scss';
 import LoginPage from '../containers/LoginPage';
-import withAuth from '../hocs/withAuth';
+import configureStore from '../store/configureStore';
+import { setCurrentUser } from '../actions/currentUser';
 
 export const history = createBrowserHistory();
 
+const store = configureStore();
+
+if (localStorage.token) {
+  const userData = decode(localStorage.token);
+  if (userData) {
+    store.dispatch(setCurrentUser({ authenticated: true, data: userData }));
+  }
+}
+
 const App = () => (
-  <Router history={history}>
-    <Switch>
-      <Route path="/login" component={LoginPage} />
-      <Route path="/" component={BooksDashboard} />
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={BooksDashboard} />
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default App;
