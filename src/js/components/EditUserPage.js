03034ff1aeeac68f0dde0580d4247bfcb4c139cc
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-03 18:01:48 +0800

    Create EditUserPage component

---
+++ src/js/components/EditUserPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import UserForm from './UserForm';
+
+const EditUserPage = () => (
+  <div>
+    <Header />
+    <h2>Edit Account</h2>
+    <UserForm />
+  </div>
+);
+
+export default EditUserPage;
