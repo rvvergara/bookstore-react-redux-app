0835c640b8b9473b1ec369aa0bc849f740400d96
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-03 12:25:34 +0800

    Set proxy in package.json

--- src/js/services/api.js
+++ src/js/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 export const fetchData = async (method, path, data) => {
-  const baseUrl = 'http://localhost:3000/v1';
-  const fullPath = `${baseUrl}/${path}`;
-  const result = await axios[method](fullPath, data);
-
+  const result = await axios[method](path, data);
   return result;
 };
 
