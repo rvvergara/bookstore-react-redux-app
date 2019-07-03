cef7a794a9ff2c2022c5adc865175b5051b7df0e
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-03 12:16:37 +0800

    Add current user full name into the Header

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../thunks/user';
 
-export const Header = ({ logout }) => (
+export const Header = ({ logout, name }) => (
   <header className="panel-bg">
     <h1 className="bookstore-cms">Bookstore App</h1>
-    <button
-      type="button"
-      className="logout-btn"
-      onClick={logout}
-    >
-      Log Out
-    </button>
+    <div className="user-wrapper">
+      <span>
+        { name }
+      </span>
+      <button
+        type="button"
+        className="logout-btn"
+        onClick={logout}
+      >
+        Log Out
+      </button>
+    </div>
   </header>
 );
 
-export default connect(null, { logout })(Header);
+const mapStateToProps = state => ({
+  name: `${state.currentUser.data.first_name} ${state.currentUser.data.last_name}`,
+});
+
+export default connect(mapStateToProps, { logout })(Header);
