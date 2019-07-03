0e3d0b9bd9267b4459326aaf79ff17722854107e
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-03 13:09:02 +0800

    Add thunk for signing up users

--- src/js/thunks/user.js
+++ src/js/thunks/user.js
@@ -2,21 +2,27 @@ import { setCurrentUser } from '../actions/user';
 import { setError } from '../actions/error';
 import { fetchData, setAuthorizationToken } from '../services/api';
 
+const setUserInStore = (user, dispatch) => {
+  const { token } = user;
+  localStorage.setItem('token', token);
+  setAuthorizationToken(token);
+  dispatch(setCurrentUser({ authenticated: true, data: user }));
+  dispatch(setError(null));
+};
+
+const setErrorInStore = (err, dispatch) => {
+  dispatch(setError(err.response.data.error));
+  localStorage.clear();
+  dispatch(setCurrentUser({ authenticated: false, data: null }));
+};
+
 export const login = loginParams => (dispatch) => {
   const path = 'sessions';
   return fetchData('post', path, loginParams).then((res) => {
     const { user } = res.data;
-    const { token } = user;
-    localStorage.setItem('token', token);
-    setAuthorizationToken(token);
-    dispatch(setCurrentUser({ authenticated: true, data: user }));
-    dispatch(setError(null));
+    setUserInStore(user, dispatch);
   })
-    .catch((err) => {
-      dispatch(setError(err.response.data.error));
-      localStorage.clear();
-      dispatch(setCurrentUser({ authenticated: false, data: null }));
-    });
+    .catch(err => setErrorInStore(err, dispatch));
 };
 
 export const logout = () => (dispatch) => {
@@ -24,3 +30,10 @@ export const logout = () => (dispatch) => {
   setAuthorizationToken(false);
   dispatch(setCurrentUser({ authenticated: false, data: null }));
 };
+
+export const signUp = signupParams => dispatch => fetchData('post', 'users', signupParams)
+  .then((res) => {
+    const { user } = res.data;
+    setUserInStore(user, dispatch);
+  })
+  .catch(err => setErrorInStore(err, dispatch));
