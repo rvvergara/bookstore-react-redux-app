74e994e6dc03fe94a47a531b4c0f106dd8561038
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-03 14:16:14 +0800

    Add tests to Header component test

--- src/js/test/components/Header.test.js
+++ src/js/test/components/Header.test.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Header } from '../../components/Header';
+import users from '../fixtures/users';
 
 describe('Header component', () => {
   let wrapper;
+  let wrapper2;
   let logout;
+  let currentUser;
 
   beforeEach(() => {
+    currentUser = { authenticated: true, data: users[0] };
     logout = jest.fn(() => Promise.resolve());
-    wrapper = shallow(<Header logout={logout} />);
+    wrapper = shallow(<Header logout={logout} currentUser={currentUser} />);
+    wrapper2 = shallow(<Header logout={logout} currentUser={{ authenticated: false, data: null }} />);
   });
 
-  test('Should render correctly', () => {
+  test('Should render correctly while authenticated', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
   test('Should log out when button is clicked', () => {
     wrapper.find('button').simulate('click');
     expect(logout).toHaveBeenCalled();
+  });
+
+  test('Should render correctly when not logged on', () => {
+    expect(wrapper2).toMatchSnapshot();
   });
 });
