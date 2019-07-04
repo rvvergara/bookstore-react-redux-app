0b8bcfef227874f44b66299113a8b977fd7bbcb1
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-04 18:00:53 +0800

    Write test for AdminDashboard component

---
+++ src/js/test/components/AdminDashboard.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AdminDashboard from '../../components/AdminDashboard';
+
+describe('AdminDashboard component', () => {
+  test('should render correctly', () => {
+    const wrapper = shallow(<AdminDashboard />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
