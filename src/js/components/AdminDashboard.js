9306d241b71846f04f96b3120e22dd0d9ca6e47f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-10 15:52:14 +0800

    Use react-tabs in AdminDashboard

--- src/js/components/AdminDashboard.js
+++ src/js/components/AdminDashboard.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import SearchForm from './SearchForm';
-import SearchResultsList from './SearchResultsList';
+import {
+  Tab, Tabs, TabList, TabPanel,
+} from 'react-tabs';
+import 'react-tabs/style/react-tabs.scss';
 
 const AdminDashboard = () => (
-  <div>
-    <SearchForm />
-    <SearchResultsList />
-  </div>
+  <Tabs>
+    <TabList>
+      <Tab>Manage Users</Tab>
+      <Tab>Manage Books</Tab>
+    </TabList>
+    <TabPanel>
+      <h2>Users</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Books</h2>
+    </TabPanel>
+  </Tabs>
 );
 
 export default AdminDashboard;
