bfa12322a8a1ccedfa3181064f29f92767bbd47f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-11 14:03:46 +0800

    Style UserPanel

--- src/js/components/UserPanel.js
+++ src/js/components/UserPanel.js
@@ -14,22 +14,35 @@ export const UserPanel = ({
     setChecked(!checked);
     updateAccount({ user: { access_level: level, username }, id });
   };
+  const badge = (level) => {
+    switch (level) {
+      case 2:
+        return 'Curator';
+      case 3:
+        return 'Owner';
+      default:
+        return 'User';
+    }
+  };
+
   return (
     <div className="user-panel">
       <div className="user-panel__details">
         <h3>{`${first_name} ${last_name}`}</h3>
-        <p>{ access_level}</p>
+        <p>{ badge(access_level) }</p>
       </div>
       {currentUserLevel > 2 && (
-      <label htmlFor="normal-switch">
-        <span>User is admin?</span>
-        <Switch
-          onChange={handleChange}
-          checked={checked}
-          className="react-switch"
-          id="normal-switch"
-        />
-      </label>
+      <div className="user-panel__access-switch">
+        <label htmlFor={`normal-switch-${id}`}>
+          <span>User is admin?</span>
+          <Switch
+            onChange={handleChange}
+            checked={checked}
+            className="react-switch"
+            id={`normal-switch-${id}`}
+          />
+        </label>
+      </div>
       )}
     </div>
   );
