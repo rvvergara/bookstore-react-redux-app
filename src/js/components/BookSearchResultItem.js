f202c67175afb53f0f46bb25fafe23b3a29b9c7b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-11 17:30:09 +0800

    Add imageLinks to BookSearchResultItem

--- src/js/components/BookSearchResultItem.js
+++ src/js/components/BookSearchResultItem.js
@@ -2,20 +2,32 @@ import React from 'react';
 
 const BookSearchResultItem = ({
   book: {
-    title, subtitle, description, authors,
+    title, subtitle, description, authors, imageLinks,
   },
-}) => (
-  <div>
-    <h3>{title}</h3>
-    <h5>{subtitle}</h5>
-    <h4>
+}) => {
+  const authorNames = authors ? authors.map(author => author).join(', ') : 'No author data';
+  return (
+    <div>
+      <h3>
+        {title}
+:
+        {' '}
+        {subtitle}
+      </h3>
+      {imageLinks && (
+      <div>
+        <img src={imageLinks.smallThumbnail} alt={title} />
+      </div>
+      )}
+      <h4>
 By:
-      {' '}
-      {authors}
-      {' '}
-    </h4>
-    <p>{description || 'No description'}</p>
-  </div>
-);
+        {' '}
+        {authorNames}
+        {' '}
+      </h4>
+      <p>{description || 'No description'}</p>
+    </div>
+  );
+};
 
 export default BookSearchResultItem;
