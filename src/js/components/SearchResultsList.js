8046ca676d8b2b17a4f471361f1cfd75cae009b6
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-12 18:29:57 +0800

    Show no-result message in SearchResultsList component

--- src/js/components/SearchResultsList.js
+++ src/js/components/SearchResultsList.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 import BookSearchResultPanel from './BookSearchResultPanel';
 import { listSearchResults } from '../actions/search';
 
-export const SearchResultsList = ({ searchResults, listSearchResults }) => {
+export const SearchResultsList = ({
+  searchResults, listSearchResults, searchTerm, error,
+}) => {
   useEffect(() => () => listSearchResults([]), [listSearchResults]);
   return (
 
@@ -12,18 +14,29 @@ export const SearchResultsList = ({ searchResults, listSearchResults }) => {
       <h4 className="search-result-heading">
         Search results for
         {' '}
-        {}
+        {searchTerm}
       </h4>
       )}
       {searchResults.map(book => (
         <BookSearchResultPanel key={book.id} book={book} />
       ))}
+      {
+        error && (
+        <h4>
+          No results for
+          {' '}
+          {searchTerm}
+        </h4>
+        )
+      }
     </div>
   );
 };
 
 const mapStateToProps = state => ({
   searchResults: state.searchResults,
+  searchTerm: state.searchTerm,
+  error: state.errors ? state.errors[0] : null,
 });
 
 export default connect(mapStateToProps, { listSearchResults })(SearchResultsList);
--- src/js/thunks/book.js
+++ src/js/thunks/book.js
@@ -1,4 +1,5 @@
 import { listSearchResults } from '../actions/search';
+import { setSearchTerm } from '../actions/searchTerm';
 import { setErrors } from '../actions/errors';
 import { bookListFromGoogle } from '../services/api';
 import getUnique from '../services/arrayProcessing';
@@ -11,10 +12,14 @@ export const searchBooks = (keyword, isAdminSearch) => (dispatch) => {
           id,
           ...volumeInfo,
         })).filter(book => book.imageLinks && book.description && book.authors);
+
         dispatch(listSearchResults(getUnique(resultsData, 'id')));
+        dispatch(setSearchTerm(keyword));
       })
       .catch(() => {
+        dispatch(setSearchTerm(keyword));
         dispatch(setErrors(['Cannot find book']));
+        dispatch(listSearchResults([]));
       });
   }
 };
