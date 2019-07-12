a3f5eda2cbd8fba985f575910d5bf018ab917126
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-12 16:58:22 +0800

    Add heading to SearchResultsList

--- src/js/components/SearchResultsList.js
+++ src/js/components/SearchResultsList.js
@@ -5,9 +5,21 @@ import { listSearchResults } from '../actions/search';
 
 export const SearchResultsList = ({ searchResults, listSearchResults }) => {
   useEffect(() => () => listSearchResults([]), [listSearchResults]);
-  return searchResults.map(book => (
-    <BookSearchResultPanel key={book.id} book={book} />
-  ));
+  return (
+
+    <div>
+      {searchResults.length > 0 && (
+      <h4 className="search-result-heading">
+        Search results for
+        {' '}
+        {}
+      </h4>
+      )}
+      {searchResults.map(book => (
+        <BookSearchResultPanel key={book.id} book={book} />
+      ))}
+    </div>
+  );
 };
 
 const mapStateToProps = state => ({
