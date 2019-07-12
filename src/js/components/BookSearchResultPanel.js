b225c1ad1fef90122130be529e798e8338df38bc
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-12 16:01:41 +0800

    Rename BookSearchResultItem to BookSearchResultPanel

--- src/js/components/BookSearchResultPanel.js
+++ src/js/components/BookSearchResultPanel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BookSearchResultItem = ({
+const BookSearchResultPanel = ({
   book: {
     id, title, subtitle, description, authors, imageLinks,
   },
@@ -38,4 +38,4 @@ const BookSearchResultItem = ({
   );
 };
 
-export default BookSearchResultItem;
+export default BookSearchResultPanel;
--- src/js/components/SearchResultsList.js
+++ src/js/components/SearchResultsList.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import BookSearchResultItem from './BookSearchResultItem';
+import BookSearchResultPanel from './BookSearchResultPanel';
 import { listSearchResults } from '../actions/search';
 
 export const SearchResultsList = ({ searchResults, listSearchResults }) => {
   useEffect(() => () => listSearchResults([]), [listSearchResults]);
   return searchResults.map(book => (
-    <BookSearchResultItem key={book.id} book={book} />
+    <BookSearchResultPanel key={book.id} book={book} />
   ));
 };
 
