29e9976dfff43633ad510b45900cb1e4da724551
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-22 16:28:49 +0800

    Update search results upon deleting from collection

--- src/js/components/Book.js
+++ src/js/components/Book.js
@@ -16,16 +16,18 @@ export const Book = ({
     const path = `/v1/users/${username}/collection`;
     addBookToLibrary(path, { collection_item: { book_id: book.id } })
       .then((res) => {
-        const { id } = res.collection_item;
-        updateSearchResult(book.id, id);
-        switchAddBookMode({ ...book, item_id: id, included: true });
-        // switchAddBookMode();
+        const { item_id } = res.collection_item;
+        updateSearchResult(book.id, item_id);
+        switchAddBookMode({ ...book, item_id, included: true });
       });
   };
 
   const removeFromCollection = () => {
     fetchRemoveBook(username, book.item_id)
-      .then(() => switchAddBookMode());
+      .then(() => {
+        updateSearchResult(book.id);
+        switchAddBookMode({ ...book, item_id: null, included: false });
+      });
   };
 
   const addBookBtn = (
